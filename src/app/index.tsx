c9d78adf9c30ad02116c6f4ad2dedd6b0a0076ca
Soradech <soradech.k@fte.kmutnb.ac.th>
2026-09-21 19:34:04 +0700

    add TextInput

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,23 +1,31 @@
 import { Buffer } from 'buffer'; // Run: npm install buffer
 import { useEffect, useState } from 'react';
 import {
+  Alert,
   Button, FlatList, PermissionsAndroid, Platform,
-  StyleSheet, Text, TouchableOpacity, View
+  StyleSheet, Text,
+  TextInput,
+  TouchableOpacity, View
 } from 'react-native';
 import { BleManager, Device } from 'react-native-ble-plx';
 
 const manager = new BleManager();
 
 // Replace these with your target device's UUIDs
-const SERVICE_UUID = "0000181C-0000-1000-8000-00805f9b34fb"; //0x181C; //'FEE0'; //'12345678-1234-5678-1234-567812345678';
-const CHAR_UUID_READ = "00002A8A-0000-1000-8000-00805f9b34fb"; //0x2AB4; //'2A19'; //'87654321-4321-4321-4321-210987654321';
-const CHAR_UUID_WRITE = '00002ab4-0000-1000-8000-00805f9b34fb';
-const CHAR_UUID_NOTIFY = 'fedcba98-fedc-fedc-fedc-fedcba987654';
+//const SERVICE_UUID = "0000181C-0000-1000-8000-00805f9b34fb"; //0x181C; //'FEE0'; //'12345678-1234-5678-1234-567812345678';
+//const CHAR_UUID_READ = "00002A8A-0000-1000-8000-00805f9b34fb"; //0x2AB4; //'2A19'; //'87654321-4321-4321-4321-210987654321';
+//const CHAR_UUID_WRITE = '00002ab4-0000-1000-8000-00805f9b34fb';
+//const CHAR_UUID_NOTIFY = 'fedcba98-fedc-fedc-fedc-fedcba987654';
+
+const SERVICE_UUID = 'aee04821-1973-4e1f-a590-e84b10d580e7';
+const CHAR_UUID_READ = 'cde07b1a-889b-44b7-a99f-c888dddac729';
+const CHAR_UUID_WRITE = 'cde07b1a-889b-44b7-a99f-c888dddac729';
 
 export default function Index() {
   const [devices, setDevices] = useState<Device[]>([]);
   const [connectedDevice, setConnectedDevice] = useState<Device | null>(null);
   const [receivedData, setReceivedData] = useState<string>('');
+  const [writeValue, setWriteValue] = useState('');
 
   async function requestPermissions() {
     manager.stopDeviceScan();
@@ -109,19 +117,25 @@ export default function Index() {
   // 4. WRITE Mode (Push Data)
   const writeCharacteristic = async () => {
     if (!connectedDevice) return;
+    if (!writeValue) {
+      Alert.alert('Input Error', 'Please enter a value to write.');
+      return;
+    } 
     try {
       // Data MUST be converted to Base64
-      const base64Value = Buffer.from('Hello BLE').toString('base64');
-      
+      let greeting = "Hello " + writeValue;
+      greeting = greeting + " from React Native BLE PLX!";
+      const base64Value = Buffer.from(greeting.toString()).toString('base64');
+      const device_id:string = connectedDevice.id.toString();
       // Use writeCharacteristicWithResponseForDevice for Write Request
       // Use writeCharacteristicWithoutResponseForDevice for Write Command
       await manager.writeCharacteristicWithResponseForDevice(
-        connectedDevice.id.toString(),
-        "00001523-1212-EFDE-1523-785FEABCD123",
+        device_id,
+        SERVICE_UUID,
         CHAR_UUID_WRITE,
         base64Value
       );
-      console.log('Write Success!');
+      console.log('Write successful:', greeting.toString());
     } catch (error) {
       console.log('Write failed:', error);
     }
@@ -177,7 +191,9 @@ export default function Index() {
           <Text style={styles.dataBox}>{receivedData || "No data fetched yet"}</Text>
           
           <Button title="Read Value" onPress={readCharacteristic} />
-          <Button title="Write 'Hello BLE'" onPress={writeCharacteristic} />
+          <TextInput style={styles.input} value={writeValue} onChange={(text) => setWriteValue(text)} 
+                placeholder="Enter value to write" />  
+          <Button title="Write Value" onPress={writeCharacteristic} />
           <Button title="Subscribe to Notifications" onPress={startNotificationStream} />
           <Button title="Disconnect" onPress={() => {
             manager.cancelDeviceConnection(connectedDevice.id);
@@ -190,10 +206,11 @@ export default function Index() {
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, paddingTop: 60, paddingHorizontal: 20, backgroundColor: '#fff' },
+  container: { flex: 1, paddingTop: 25, paddingHorizontal: 20, backgroundColor: '#fff' },
   deviceRow: { padding: 15, marginVertical: 5, backgroundColor: '#f0f0f0', borderRadius: 5 },
-  dashboard: { gap: 15 },
+  dashboard: { gap: 10 },
   title: { fontSize: 18, fontWeight: 'bold' },
-  dataBox: { padding: 20, backgroundColor: '#eef', marginVertical: 10, textAlign: 'center' }
+  dataBox: { padding: 10, backgroundColor: '#eef', marginVertical: 10, textAlign: 'center' },
+  input: { padding: 10, borderColor: '#aaf', color: '#000', borderWidth: 1, marginVertical: 10, borderRadius: 5 }
 });
 
